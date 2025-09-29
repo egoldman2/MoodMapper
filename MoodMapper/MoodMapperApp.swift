5c3232f73a981b0889626e8f5c8cb7b52e7e2de5
egoldman2 <ethan.goldman@student.uts.edu.au>
2025-09-29 14:24:04 +1000

    Initial push - Implement base structure - Implement firebase - Initial project setup

--- MoodMapper/MoodMapperApp.swift
+++ MoodMapper/MoodMapperApp.swift
@@ -7,10 +7,22 @@
 
 import SwiftUI
 import CoreData
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct MoodMapperApp: App {
     let persistenceController = PersistenceController.shared
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
 
     var body: some Scene {
         WindowGroup {
@@ -19,3 +31,4 @@ struct MoodMapperApp: App {
         }
     }
 }
+
